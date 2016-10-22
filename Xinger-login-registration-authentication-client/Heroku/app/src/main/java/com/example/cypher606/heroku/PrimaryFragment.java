4da3b7baa83e24657812e4d30fd107195460ba93
0xFALLm <ashimrajkonwar@gmail.com>
2016-10-22 20:01:19 +0530

    Added logo

---
+++ Xinger-login-registration-authentication-client/Heroku/app/src/main/java/com/example/cypher606/heroku/PrimaryFragment.java
@@ -0,0 +1,22 @@
+package com.example.cypher606.heroku;
+
+/**
+ * Created by cypher606 on 22/10/16.
+ */
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class PrimaryFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.primary_layout,null);
+    }
+}
