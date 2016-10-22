b7618123dfbbcc504afdd3ee70e764c91b3f3823
0xFALLm <ashimrajkonwar@gmail.com>
2016-10-23 00:21:17 +0530

    Master

---
+++ Xinger-login-registration-authentication-client/MakeMe/app/src/main/java/com/xinger/model/Response.java
@@ -0,0 +1,15 @@
+package com.xinger.model;
+
+public class Response {
+
+    private String message;
+    private String token;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
