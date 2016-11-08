62a28ff72fc832425bbd319f824de0f9d182dcfb
0xFALLm <ashimrajkonwar@gmail.com>
2016-11-08 20:50:42 +0530

    updated dashboard

--- Xinger/app/src/main/java/com/learn2crack/DashboardActivity.java
+++ Xinger/app/src/main/java/com/learn2crack/DashboardActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -67,6 +69,9 @@ public class DashboardActivity extends AppCompatActivity
     private CompositeSubscription mSubscriptions;
 
 
+
+    FragmentManager mFragmentManager;
+    FragmentTransaction mFragmentTransaction;
 
 
 
@@ -243,8 +248,10 @@ public class DashboardActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
+        if (id == R.id.nav_profile) {
+            mFragmentManager = getSupportFragmentManager();
+            mFragmentTransaction = mFragmentManager.beginTransaction();
+            mFragmentTransaction.replace(R.id.tabLoad, new ProfileFragment()).commit();
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
